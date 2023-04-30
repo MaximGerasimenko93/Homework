20ebefc8d21ad476cd43a06ca2472de2f9260e5d
MaximGerasimenko93 <gerasimx93@gmail.com>
2023-04-30 13:09:28 +0700

    Добавил моки

--- src/test/java/test/GameTest.java
+++ src/test/java/test/GameTest.java
@@ -2,52 +2,47 @@ package test;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.game.*;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-
+@ExtendWith(MockitoExtension.class)
 public class GameTest {
 
-    private Dice dice = new DiceImpl();
-    private GameWinnerPrinter winnerPrinter = new GameWinnerConsolePrinter();
-    Game game = new Game(dice, winnerPrinter);
+    @Mock
+    Dice dice;
+    @Mock
+    GameWinnerPrinter winnerPrinter;
+    @InjectMocks
+    Game game;
     Player player1 = new Player("TestName1");
     Player player2 = new Player("TestName2");
 
     @Test
-    @DisplayName("Проверка на ничью")
-    public void testEqualResult() {
+    @DisplayName("Победа первого игрока")
+    public void testWhenFirstWinnerPlayer() {
+        when(dice.roll()).thenReturn(6).thenReturn(4);
         game.playGame(player1, player2);
-        int resultPlayer1 = dice.roll();
-        int resultPlayer2 = dice.roll();
-
-        assertNotEquals(resultPlayer1, resultPlayer2, "Ничья! Перебросьте кубики");
+        verify(winnerPrinter).printWinner(eq(player1));
     }
 
     @Test
-    @DisplayName("Победа первого игрока")
-    public void testWhenFirstWinnerPlayer() {
-
+    @DisplayName("Победа второго игрока")
+    public void testWhenSecondWinnerPlayer() {
+        when(dice.roll()).thenReturn(5).thenReturn(6);
+        game.playGame(player1, player2);
+        verify(winnerPrinter).printWinner(eq(player2));
     }
 
-//    @Test
-//    public void testIfFirstPlayerWinner() {
-//        game.playGame(player1, player2);
-//        int resultPlayer1 = dice.roll();
-//        int resultPlayer2 = dice.roll();
-//
-//        assertTrue(resultPlayer1 > resultPlayer2, "Победа второго игрока");
-//    }
-//
-//    @Test
-//    public void testIfSecondPlayerWinner() {
-//        game.playGame(player1, player2);
-//        int resultPlayer1 = dice.roll();
-//        int resultPlayer2 = dice.roll();
-//
-//        assertTrue(resultPlayer2 > resultPlayer1, "Победа первого игрока");
-//    }
+    @Test
+    @DisplayName("Ничья")
+    public void testWhenEqualsResult() {
+        when(dice.roll()).thenReturn(5).thenReturn(5);
+        game.playGame(player1, player2);
+        verify(winnerPrinter).printWinner(eq(player2));
+    }
 }
 
